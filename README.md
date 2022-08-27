920d1592c1a5ed2d4f3ae0afbedaced92e8d2198
Gian Franco Fagotti <gianffagotti@gmail.com>
2022-08-27 13:23:22 -0300

    readme y scripts

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
-# AmediaTestCrud
+Test Técnico Amedia
+
+Para utilizar el sitio web, deben actualizar el string de conexión en scr\AmediaTestCrud.Web -> appsettings.json
+
+Con la tablas de tRol y tUsers ya se puede utilizar correctamente. Es necesario tener en cuenta que se considera al rol 'Admininstrador' con el ID = 1. También para poder iniciar sesión, el usuario debe estar activo, es decir sn_activo = 1.
+
+_____________________________________
+
+Stored Procedures:
+
+Partiendo de que ya existen las tablas tUsers, tPelicula, tGenero y tGeneroPelicula, ejecutar script de migración en scripts -> script_test_migration.sql
+
+1) Crear usuarios, cuyo documento no exista actualmente en la base de datos, en
+caso de existir, debería devolver un mensaje de error, en caso contrario insertarlo
+	-> Sp_tusers_a
+2) Crear/Borrar/Modificar peliculas (Borrar es poner en 0 el stock de ventas y
+alquileres)
+	-> Sp_tpelicula_a | Sp_tpelicula_b | Sp_tpelicula_m
+3) Crear géneros
+	-> Sp_tgenero_a
+4) Asignar géneros a películas, verificar que la película no tenga asignada el
+género previamente.
+	-> Sp_tgeneropelicula_a
+5) Alquilar y Vender películas
+	-> Sp_alquilar_pelicula | Sp_venta_pelicula
+6) Obtener las películas en stock para alquiler
+	-> Sp_tpeliculas_alquiler_en_stock
+7) Obtener las películas en stock para vender
+	-> Sp_tpeliculas_venta_en_stock
+8) Alquilar película
+	-> Sp_alquilar_pelicula
+9) Vender pelicula
+	-> Sp_venta_pelicula
+10) Devolver película
+	-> Sp_tpeliculas_devolver
+11) Ver películas que no fueron devueltas y que usuarios la tienen
+	-> Sp_tpeliculas_sin_devolver
+12) Ver qué películas fueron alquiladas por usuario y cuánto pagó y que día
+	-> Sp_tpeliculas_alquilada_por_usuario
+13) Ver todas las películas, cuantas veces fueron alquiladas y cuanto se recaudo por ellas
+	-> Sp_tpeliculas_alquiler_estadistica
--- scripts/script_test_migration.sql
+++ scripts/script_test_migration.sql
@@ -63,3 +63,377 @@ GO
 
 ALTER TABLE [dbo].[tPeliculaVenta] CHECK CONSTRAINT [FK_tPeliculaVenta_tUsers]
 GO
+
+---------------------------------------------------------------------------------------------------------------
+  
+/*--------------------------------------------------------------------------------------------------------------------                
+-- Autor: Gian                
+-- Fecha de creación: 27/08/2022    
+-- Descripción: Alquiler de pelicula  
+--                
+-- Modificaciones:                
+-- Descripción:                
+-- Autor:                
+-- Fecha:                
+--                
+----------------------------------------------------------------------------------------------------------------------*/  
+CREATE PROCEDURE [dbo].[Sp_alquilar_pelicula] @cod_pelicula    INT,  
+                                             @cod_usuario     INT,  
+                                             @precio_alquiler NUMERIC(18, 2)  
+AS  
+    UPDATE tPelicula  
+    SET    cant_disponibles_alquiler = cant_disponibles_alquiler - 1  
+    WHERE  cod_pelicula = @cod_pelicula  
+  
+    INSERT INTO [dbo].[tPeliculaAlquiler]  
+                ([cod_usuario],  
+                 [cod_pelicula],  
+                 [precio_alquiler],  
+                 [fecha_alquiler],  
+                 [devuelta])  
+    VALUES      ( @cod_usuario,  
+                  @cod_pelicula,  
+                  @precio_alquiler,  
+                  Getdate(),  
+                  0)   
+GO
+
+/*--------------------------------------------------------------------------------------------------------------------                
+-- Autor: Gian                
+-- Fecha de creación: 27/08/2022    
+-- Descripción: Alta de generos  
+--                
+-- Modificaciones:                
+-- Descripción:                
+-- Autor:                
+-- Fecha:                
+--                
+----------------------------------------------------------------------------------------------------------------------*/  
+CREATE PROCEDURE [dbo].[Sp_tgenero_a] @cod_genero INT out,  
+                                      @txt_desc   VARCHAR(500)  
+AS  
+    INSERT INTO [dbo].[tGenero]  
+                ([txt_desc])  
+    VALUES      (@txt_desc)  
+  
+    SET @cod_genero = @@IDENTITY   
+GO
+
+/*--------------------------------------------------------------------------------------------------------------------                
+-- Autor: Gian                
+-- Fecha de creación: 27/08/2022    
+-- Descripción: Alta de generos a peliculas  
+--                
+-- Modificaciones:                
+-- Descripción:                
+-- Autor:                
+-- Fecha:                
+--                
+----------------------------------------------------------------------------------------------------------------------*/  
+CREATE PROCEDURE [dbo].[Sp_tgeneropelicula_a] @cod_pelicula INT,  
+                                              @cod_genero   INT  
+AS  
+    IF NOT EXISTS (SELECT *  
+                   FROM   tGeneroPelicula  
+                   WHERE  cod_pelicula = @cod_pelicula  
+                          AND cod_genero = @cod_genero)  
+      BEGIN  
+          INSERT INTO [dbo].[tGeneroPelicula]  
+                      ([cod_pelicula],  
+                       [cod_genero])  
+          VALUES      (@cod_pelicula,  
+                       @cod_genero)  
+      END   
+GO
+
+/*--------------------------------------------------------------------------------------------------------------------                
+-- Autor: Gian                
+-- Fecha de creación: 27/08/2022    
+-- Descripción: Alta de peliculas  
+--                
+-- Modificaciones:                
+-- Descripción:                
+-- Autor:                
+-- Fecha:                
+--                
+----------------------------------------------------------------------------------------------------------------------*/  
+CREATE PROCEDURE [dbo].[Sp_tpelicula_a] @cod_pelicula              INT out,  
+                                       @txt_desc                  VARCHAR(500),  
+                                       @cant_disponibles_alquiler INT,  
+                                       @cant_disponibles_venta    INT,  
+                                       @precio_alquiler           NUMERIC(18, 2),  
+                                       @precio_venta              NUMERIC(18, 2)  
+AS  
+    INSERT INTO [dbo].[tPelicula]  
+                ([txt_desc],  
+                 [cant_disponibles_alquiler],  
+                 [cant_disponibles_venta],  
+                 [precio_alquiler],  
+                 [precio_venta])  
+    VALUES      (@txt_desc,  
+                 @cant_disponibles_alquiler,  
+                 @cant_disponibles_venta,  
+                 @precio_alquiler,  
+                 @precio_venta )  
+  
+    SET @cod_pelicula = @@IDENTITY   
+GO
+
+/*--------------------------------------------------------------------------------------------------------------------                
+-- Autor: Gian                
+-- Fecha de creación: 27/08/2022    
+-- Descripción: Borrado lógico de peliculas  
+--                
+-- Modificaciones:                
+-- Descripción:                
+-- Autor:                
+-- Fecha:                
+--                
+----------------------------------------------------------------------------------------------------------------------*/  
+CREATE PROCEDURE [dbo].[Sp_tpelicula_b] @cod_pelicula INT  
+AS  
+    UPDATE [dbo].[tPelicula]  
+    SET    [cant_disponibles_alquiler] = 0,  
+           [cant_disponibles_venta] = 0  
+    WHERE  cod_pelicula = @cod_pelicula  
+GO
+
+/*--------------------------------------------------------------------------------------------------------------------                
+-- Autor: Gian                
+-- Fecha de creación: 27/08/2022    
+-- Descripción: Modificación de peliculas  
+--                
+-- Modificaciones:                
+-- Descripción:                
+-- Autor:                
+-- Fecha:                
+--                
+----------------------------------------------------------------------------------------------------------------------*/  
+CREATE PROCEDURE [dbo].[Sp_tpelicula_m] @cod_pelicula              INT,  
+                                        @txt_desc                  VARCHAR(500),  
+                                        @cant_disponibles_alquiler INT,  
+                                        @cant_disponibles_venta    INT,  
+                                        @precio_alquiler           NUMERIC(18, 2),  
+                                        @precio_venta              NUMERIC(18, 2)  
+AS  
+    UPDATE [dbo].[tPelicula]  
+    SET    [txt_desc] = @txt_desc,  
+           [cant_disponibles_alquiler] = @cant_disponibles_alquiler,  
+           [cant_disponibles_venta] = @cant_disponibles_venta,  
+           [precio_alquiler] = @precio_alquiler,  
+           [precio_venta] = @precio_venta  
+    WHERE  cod_pelicula = @cod_pelicula  
+GO
+
+/*--------------------------------------------------------------------------------------------------------------------                
+-- Autor: Gian                
+-- Fecha de creación: 27/08/2022    
+-- Descripción: Listado de peliculas alquiladas por un usuario  
+--                
+-- Modificaciones:                
+-- Descripción:                
+-- Autor:                
+-- Fecha:                
+--                
+----------------------------------------------------------------------------------------------------------------------*/  
+CREATE PROCEDURE [dbo].[Sp_tpeliculas_alquilada_por_usuario] @cod_usuario INT  
+AS  
+    SELECT tPelicula.cod_pelicula,  
+           tPelicula.txt_desc,  
+           tPeliculaAlquiler.precio_alquiler,  
+           tPeliculaAlquiler.fecha_alquiler,  
+           tPeliculaAlquiler.devuelta  
+    FROM   tPeliculaAlquiler  
+           INNER JOIN tPelicula  
+                   ON tPeliculaAlquiler.cod_pelicula = tPelicula.cod_pelicula  
+           INNER JOIN tUsers  
+                   ON tPeliculaAlquiler.cod_usuario = tUsers.cod_usuario  
+    WHERE  tPeliculaAlquiler.cod_usuario = @cod_usuario   
+
+GO
+
+/*--------------------------------------------------------------------------------------------------------------------                
+-- Autor: Gian                
+-- Fecha de creación: 27/08/2022    
+-- Descripción: Peliculas en stock para alquilar  
+--                
+-- Modificaciones:                
+-- Descripción:                
+-- Autor:                
+-- Fecha:                
+--                
+----------------------------------------------------------------------------------------------------------------------*/  
+CREATE PROCEDURE [dbo].[Sp_tpeliculas_alquiler_en_stock]  
+AS  
+    SELECT *  
+    FROM   tPelicula  
+    WHERE  cant_disponibles_alquiler > 0   
+GO
+
+/*--------------------------------------------------------------------------------------------------------------------                
+-- Autor: Gian                
+-- Fecha de creación: 27/08/2022    
+-- Descripción: Estadistica de alquiler de peliculas  
+--                
+-- Modificaciones:                
+-- Descripción:                
+-- Autor:                
+-- Fecha:                
+--                
+----------------------------------------------------------------------------------------------------------------------*/  
+CREATE PROCEDURE [dbo].[Sp_tpeliculas_alquiler_estadistica]  
+AS  
+    SELECT tPelicula.cod_pelicula,  
+           tPelicula.txt_desc,  
+           Count(1)                               AS cantidad_alquiladas,  
+           Sum(tPeliculaAlquiler.precio_alquiler) AS total_recaudado  
+    FROM   tPeliculaAlquiler  
+           INNER JOIN tPelicula  
+                   ON tPeliculaAlquiler.cod_pelicula = tPelicula.cod_pelicula  
+    GROUP  BY tPelicula.cod_pelicula,  
+              tPelicula.txt_desc
+GO
+
+  
+/*--------------------------------------------------------------------------------------------------------------------                
+-- Autor: Gian                
+-- Fecha de creación: 27/08/2022    
+-- Descripción: Devolver pelicula alquilada  
+--                
+-- Modificaciones:                
+-- Descripción:                
+-- Autor:                
+-- Fecha:                
+--                
+----------------------------------------------------------------------------------------------------------------------*/  
+CREATE PROCEDURE [dbo].[Sp_tpeliculas_devolver] @cod_pelicula INT,  
+                                               @cod_usuario  INT  
+AS  
+    UPDATE tPelicula  
+    SET    cant_disponibles_alquiler = cant_disponibles_alquiler + 1  
+    WHERE  cod_pelicula = @cod_pelicula  
+  
+    UPDATE tPeliculaAlquiler  
+    SET    devuelta = 1  
+    WHERE  cod_pelicula = @cod_pelicula  
+           AND cod_usuario = @cod_usuario   
+GO
+
+/*--------------------------------------------------------------------------------------------------------------------                
+-- Autor: Gian                
+-- Fecha de creación: 27/08/2022    
+-- Descripción: Listado de peliculas con sus usuarios sin devolver  
+--                
+-- Modificaciones:                
+-- Descripción:                
+-- Autor:                
+-- Fecha:                
+--                
+----------------------------------------------------------------------------------------------------------------------*/  
+CREATE PROCEDURE [dbo].[Sp_tpeliculas_sin_devolver]  
+AS  
+    SELECT tPelicula.cod_pelicula,  
+           tPelicula.txt_desc,  
+           tUsers.cod_usuario,  
+           tUsers.txt_nombre,  
+           tUsers.txt_apellido  
+    FROM   tPeliculaAlquiler  
+           INNER JOIN tPelicula  
+                   ON tPeliculaAlquiler.cod_pelicula = tPelicula.cod_pelicula  
+           INNER JOIN tUsers  
+                   ON tPeliculaAlquiler.cod_usuario = tUsers.cod_usuario  
+    WHERE  tPeliculaAlquiler.devuelta = 0   
+GO
+
+/*--------------------------------------------------------------------------------------------------------------------                
+-- Autor: Gian                
+-- Fecha de creación: 27/08/2022    
+-- Descripción: Peliculas en stock para vender  
+--                
+-- Modificaciones:                
+-- Descripción:                
+-- Autor:                
+-- Fecha:                
+--                
+----------------------------------------------------------------------------------------------------------------------*/  
+CREATE PROCEDURE [dbo].[Sp_tpeliculas_venta_en_stock]  
+AS  
+    SELECT *  
+    FROM   tPelicula  
+    WHERE  cant_disponibles_venta > 0   
+GO
+
+/*--------------------------------------------------------------------------------------------------------------------                
+-- Autor: Gian                
+-- Fecha de creación: 27/08/2022    
+-- Descripción: Alta de usuario validando documento              
+--                
+-- Modificaciones:                
+-- Descripción:                
+-- Autor:                
+-- Fecha:                
+--                
+----------------------------------------------------------------------------------------------------------------------*/  
+CREATE PROCEDURE [dbo].[Sp_tusers_a] @txt_user     VARCHAR(50),  
+                                     @txt_password VARCHAR(50),  
+                                     @txt_nombre   VARCHAR(200),  
+                                     @txt_apellido VARCHAR(200),  
+                                     @nro_doc      VARCHAR(50),  
+                                     @cod_rol      INT,  
+                                     @sn_activo    INT  
+AS  
+    IF EXISTS (SELECT *  
+               FROM   tUsers  
+               WHERE  nro_doc = @nro_doc)  
+      BEGIN  
+          RAISERROR('No se puede insertar el registro, documento ya existente',16,1)  
+      END  
+    ELSE  
+      BEGIN  
+          INSERT INTO [dbo].[tUsers]  
+                      ([txt_user],  
+                       [txt_password],  
+                       [txt_nombre],  
+                       [txt_apellido],  
+                       [nro_doc],  
+                       [cod_rol],  
+                       [sn_activo])  
+          VALUES      (@txt_user,  
+                       @txt_password,  
+                       @txt_nombre,  
+                       @txt_apellido,  
+                       @nro_doc,  
+                       @cod_rol,  
+                       @sn_activo)  
+      END   
+GO
+
+/*--------------------------------------------------------------------------------------------------------------------                
+-- Autor: Gian                
+-- Fecha de creación: 27/08/2022    
+-- Descripción: Venta de pelicula  
+--                
+-- Modificaciones:                
+-- Descripción:                
+-- Autor:                
+-- Fecha:                
+--                
+----------------------------------------------------------------------------------------------------------------------*/  
+CREATE PROCEDURE [dbo].[Sp_venta_pelicula] @cod_pelicula INT,  
+                                           @cod_usuario  INT,  
+                                           @precio_venta NUMERIC(18, 2)  
+AS  
+    UPDATE tPelicula  
+    SET    cant_disponibles_venta = cant_disponibles_venta - 1  
+    WHERE  cod_pelicula = @cod_pelicula  
+  
+    INSERT INTO [dbo].[tPeliculaVenta]  
+                ([cod_usuario],  
+                 [cod_pelicula],  
+                 [precio_venta],  
+                 [fecha_venta])  
+    VALUES      ( @cod_usuario,  
+                  @cod_pelicula,  
+                  @precio_venta,  
+                  Getdate())   
+GO
